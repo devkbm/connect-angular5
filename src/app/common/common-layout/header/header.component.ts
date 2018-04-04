b2607075c5df63eaf2001e2f1d9330908df2b2ee
kbm <kbm0417@nate.com>
2018-04-04 23:39:43 +0900

    메뉴 조회 수정중

--- src/app/common/common-layout/header/header.component.ts
+++ src/app/common/common-layout/header/header.component.ts
@@ -3,6 +3,8 @@ import { AppAlarmService } from '../../common-service/app-alarm.service';
 import { MenuService } from '../../common-service/menu.service';
 import { MenuGroup } from '../../common-service/menu-group';
 import { ResponseObject } from '../../common-service/model/response-object';
+import { MenuHierarchy } from '../../common-service/menu-hierarchy';
+import { ResponseList } from '../../common-service/model/response-list';
 
 @Component({
   selector: 'app-header',
@@ -30,6 +32,31 @@ export class HeaderComponent implements OnInit {
         (model: ResponseObject<MenuGroup>) => {
           if ( model.total > 0 ) {
 
+          } else {
+
+          }
+        },
+        (err) => {
+          console.log(err);
+        },
+        () => {
+          console.log('완료');
+        }
+      );
+
+  }
+
+  // 직접 메뉴를 조회하는게 아니라 이벤트 발생해야함
+  // 메뉴트리 컴포넌트에서 메뉴가 조회되도록 수정 필요
+  public getMenuList(menuGroupCode: string) {
+    console.log(menuGroupCode);
+
+    this.menuService
+      .getMenuHierarchy(menuGroupCode)
+      .subscribe(
+        (model: ResponseList<MenuHierarchy>) => {
+          if ( model.total > 0 ) {
+            console.log(model.data);
           } else {
 
           }
